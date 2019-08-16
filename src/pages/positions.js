7deeb479079a146f37fa9836338d834c2a8765c5
hernanjkd <hernanjkd@gmail.com>
2019-08-16 12:59:25 -0400

    positions page

--- src/pages/positions.js
+++ src/pages/positions.js
@@ -7,25 +7,33 @@ import Temp from "../images/positions.png"
 const Positions = () => (
     <Layout>
 
+        <div className="positions-top-image text-light text-center py-10">
+            
+            <h1><strong>Every Job In <span className="text-brightblue">
+                One Place</span></strong></h1>
+            <h4><strong>Your <span className="text-brightblue">
+                Dream Job</span> Just A Click Away!</strong></h4>
+        </div>
+
         <div className="container text-center">
 
             <div className="row">
-                <div className="col">
+                <div className="col m-3 mt-5">
                     <img src={Temp} />
                 </div>
             </div>
             <div className="row">
-                <div className="col">
+                <div className="col m-3">
                     <img src={Temp} />
                 </div>
             </div>
             <div className="row">
-                <div className="col">
+                <div className="col m-3">
                     <img src={Temp} />
                 </div>
             </div>
             <div className="row">
-                <div className="col">
+                <div className="col m-3 mb-5">
                     <img src={Temp} />
                 </div>
             </div>
