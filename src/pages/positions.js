d79e5afd0f0ed529344f5c6c636a9390d237b728
hernanjkd <hernanjkd@gmail.com>
2019-08-18 02:27:36 -0400

    fonts awesome

--- src/pages/positions.js
+++ src/pages/positions.js
@@ -1,22 +1,37 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from 'react'
+import Layout from '../components/layout'
 
 //assets
-import Temp from "../images/positions.png"
+import Temp from '../images/positions.png'
 
 const Positions = () => (
     <Layout>
-
         <div className="positions-top-image text-light text-center py-10">
-            
-            <h1><strong>Every Job In <span className="text-brightblue">
-                One Place</span></strong></h1>
-            <h4><strong>Your <span className="text-brightblue">
-                Dream Job</span> Just A Click Away!</strong></h4>
+            <h1>
+                <strong>
+                    Every Job In
+                    <span className="text-brightblue"> One Place</span>
+                </strong>
+            </h1>
+            <h4>
+                <strong>
+                    Your <span className="text-brightblue">Dream Job</span> Just
+                    A Click Away!
+                </strong>
+            </h4>
+            <div>Want to find a job? We have 263</div>
+        </div>
+
+        <div className="position-search">
+            <i class="fas fa-map-marker-alt fa-xs text-purple"></i>
+            <span className="text-muted font-small">Miami Beach</span>
+            <i class="far fa-calendar-check fa-xs text-purple"></i>
+            <span className="text-muted font-small">
+                $11.53 - $12.03 / Hour
+            </span>
         </div>
 
         <div className="container text-center">
-
             <div className="row">
                 <div className="col m-3 mt-5">
                     <img src={Temp} />
@@ -37,9 +52,7 @@ const Positions = () => (
                     <img src={Temp} />
                 </div>
             </div>
-
         </div>
-
     </Layout>
 )
 
