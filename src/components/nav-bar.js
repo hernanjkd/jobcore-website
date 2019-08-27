99146212eb5516e85d972f1cb5ad1f1e3f4a1d21
hernanjkd <hernanjkd@gmail.com>
2019-08-26 23:57:50 -0400

    checking empty nav

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -6,47 +6,7 @@ import Logo from '../images/jobcore-logo.png'
 
 const Navbar = () => (
     <nav className="navbar navbar-expand-lg justify-content-between px-10">
-        <Link to="/">
-            <a className="navbar-brand" href="#">
-                <img src={Logo} />
-            </a>
-        </Link>
 
-        <div>
-            <div className="text-right">
-                <small>
-                    <span className="pr-4">(305)555-8473</span>
-                    <a className="link px-2 pr-4" href="#">
-                        <i class="fas fa-caret-right text-brightblue mr-2"></i>
-                        My Account
-                    </a>
-                    <a className="link px-2" href="#">
-                        <i class="fas fa-caret-right text-brightblue mr-2"></i>
-                        Sign In
-                    </a>
-                </small>
-            </div>
-            <div>
-                <Link to="/job-seekers">
-                    <a className="link px-4" href="#">
-                        Job Seekers
-                        <span className="sr-only">(current)</span>
-                    </a>
-                </Link>
-                <Link to="/positions">
-                    <a className="link px-4" href="#">
-                        Positions
-                    </a>
-                </Link>
-
-                <button
-                    className="btn radius btn-darkgreen my-2 ml-5 px-4"
-                    type="submit"
-                >
-                    Get Started
-                </button>
-            </div>
-        </div>
     </nav>
 )
 
