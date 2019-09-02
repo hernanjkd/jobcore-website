9cc4bed0e4c4f9ba75f6e8c9596a90cb3822452b
hernanjkd <hernanjkd@gmail.com>
2019-09-02 18:34:46 -0400

    login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Layout from '../components/layout'
+
+/* eslint-disable */
 
 const Login = () => (
     <Layout>
@@ -13,8 +16,40 @@ const Login = () => (
             </div>
         </div>
 
-        <div className="container-fluid text-center my-5 py-5">
-
+        <div className="container my-5">
+            <div className="mx-auto w-400px py-5">
+                <h2>Login</h2>
+                <p className="text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <h6 className="mt-5">Email</h6>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="example@gmail.com"
+                />
+                <h6 className="mt-4">Password</h6>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="password"
+                />
+                <div className="pt-3 text-gray">
+                    <small>
+                        <input type="checkbox" /> Remember Password
+                    </small>
+                </div>
+                <button
+                    className="btn radius btn-purple mt-5 ml-3 px-5 py-2"
+                >
+                    Login
+                </button>
+                <div className="pt-3">
+                    <small className="text-secondary">Don't have an account?
+                        <Link to="/sign-up">
+                            <a className="text-success" href=""> Sign Up Now</a>
+                        </Link>
+                    </small>
+                </div>
+            </div>
         </div>
     </Layout>
 )
