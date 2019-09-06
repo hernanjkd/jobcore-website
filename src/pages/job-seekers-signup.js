0e3dd5fb3bacd16998c89eb3f4c0e9efb7cb14ba
hernanjkd <hernanjkd@gmail.com>
2019-09-05 23:23:02 -0400

    job seekers sign up

--- src/pages/job-seekers-signup.js
+++ src/pages/job-seekers-signup.js
@@ -21,7 +21,7 @@ const JobSeekersSignUp = () => (
                 <h2 className="text-brightblue mb-5">Sign Up Now</h2>
 
                 <form>
-                    <div className="form-group py-2">
+                    <div className="form-group py-1">
                         <label className=""><h6>First Name</h6></label>
                         <input
                             type="text"
@@ -29,7 +29,7 @@ const JobSeekersSignUp = () => (
                             placeholder="John"
                         />
                     </div>
-                    <div className="form-group py-2">
+                    <div className="form-group py-1">
                         <label className=""><h6>Last Name</h6></label>
                         <input
                             type="text"
@@ -37,31 +37,50 @@ const JobSeekersSignUp = () => (
                             placeholder="Doe"
                         />
                     </div>
-                    <h6 className="mt-4">Password</h6>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="password"
-                    />
-                    <div className="pt-3 text-gray">
-                        <small>
-                            <input type="checkbox" /> Remember Password
-                        </small>
+                    <div className="form-group py-1">
+                        <label className=""><h6>Your Email</h6></label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="example@gmail.com"
+                        />
+                    </div>
+                    <div className="form-group py-1">
+                        <label className=""><h6>Cell Number</h6></label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="3055557475"
+                        />
                     </div>
                     <button
                         className="btn radius btn-purple mt-5 ml-3 px-5 py-2"
                     >
-                        Login
+                        SIGN UP
                     </button>
+                    <div className="pt-4 text-gray">
+                        <small>
+                            By clicking the button above you agree to the<br />
+                            <Link to="/terms">
+                                <a> Terms of Service </a>
+                            </Link>
+                            and
+                            <Link to="/privacy">
+                                <a> Privacy Policy</a>
+                            </Link>
+                            .
+                            <div className="d-flex justify-content-start mt-3">
+                                <input className="mt-1 mr-2" type="checkbox" />
+                                <div>
+                                    I consent to receive updates and agree to receive occasional
+                                    automated text message from JobCore. Message and data rates may
+                                    apply. Text STOP to cancel or HELP for help.
+                                </div>
+                            </div>
+                        </small>
+                    </div>
                 </form>
 
-                <div className="pt-3">
-                    <small className="text-secondary">Don't have an account?
-                        <Link to="/sign-up">
-                            <a className="text-success" href=""> Sign Up Now</a>
-                        </Link>
-                    </small>
-                </div>
             </div>
         </div>
     </Layout>
